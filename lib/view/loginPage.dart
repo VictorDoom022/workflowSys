35c037fc848517646c6d53402641aed91c54a278
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2021-07-25 18:26:11 +0800

    login page front end

--- lib/view/loginPage.dart
+++ lib/view/loginPage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class loginPage extends StatefulWidget {
@@ -8,8 +9,64 @@ class loginPage extends StatefulWidget {
 }
 
 class _loginPageState extends State<loginPage> {
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: CupertinoNavigationBar(
+        middle: Text('Login'),
+      ),
+      body: Container(
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Login',
+                  style: TextStyle(
+                      fontSize: 50
+                  ),
+                ),
+                CupertinoTextField(
+                  placeholder: 'Email',
+                  controller: emailController,
+                ),
+                SizedBox(height: 10),
+                CupertinoTextField(
+                  placeholder: 'Password',
+                  controller: passwordController,
+                  obscureText: true,
+                ),
+                SizedBox(height: 10),
+                CupertinoButton(
+                  onPressed: () async{
+
+                  },
+                  color: Colors.blue,
+                  child: Text('Login'),
+                ),
+                SizedBox(height: 20),
+                CupertinoButton(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Register'),
+                      ],
+                    ),
+                    onPressed: (){
+
+                    }
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
