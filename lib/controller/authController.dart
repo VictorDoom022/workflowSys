631a3883fb2eb8a3b8d07c3e7bb99a99e83de235
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2021-07-25 23:50:15 +0800

    add try catch for error handling

--- lib/controller/authController.dart
+++ lib/controller/authController.dart
@@ -6,30 +6,39 @@ import 'package:workflow_sys/controller/setupDir.dart';
 import 'package:workflow_sys/model/AuthReceiver.dart';
 
 void login(BuildContext context, String email, String password) async {
-  var dio = new Dio();
 
-  var response = await dio.post(
-      apiURL + "/login",
-      data: {'email' : email, 'password' : password},
-      options: Options(
-        responseType: ResponseType.json,
-        sendTimeout: 5000,
-        headers: {
-          'Accept': 'application/json',
-        }
-      )
-  );
+  try{
+    var dio = new Dio();
 
-  if(response.statusCode == 200){
-    AuthReceiver authReceiver = AuthReceiver.fromJson(response.data);
+    var response = await dio.post(
+        apiURL + "/login",
+        data: {'email' : email, 'password' : password},
+        options: Options(
+            responseType: ResponseType.json,
+            sendTimeout: 5000,
+            headers: {
+              'Accept': 'application/json',
+            }
+        )
+    );
 
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    sharedPreferences.setString("UserToken", authReceiver.token);
-    sharedPreferences.setString("UserEmail", authReceiver.user.email);
-    sharedPreferences.setString("UserName", authReceiver.user.name);
-  }else if(response.statusCode == 201){
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('These credentials do not match our records.')));
-  }else{
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Somethings went wrong. Code:' + response.statusCode.toString())));
+    if(response.statusCode == 200){
+      AuthReceiver authReceiver = AuthReceiver.fromJson(response.data);
+
+      SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+      sharedPreferences.setString("UserToken", authReceiver.token);
+      sharedPreferences.setString("UserEmail", authReceiver.user.email);
+      sharedPreferences.setString("UserName", authReceiver.user.name);
+    }else if(response.statusCode == 201){
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('These credentials do not match our records.')));
+    }else{
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Somethings went wrong. Code:' + response.statusCode.toString())));
+    }
+  }on DioError catch(e){
+    if(e.response.statusCode == 422){
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Invalid format!')));
+    }else{
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Somethings went wrong. Code' + e.response.statusCode.toString())));
+    }
   }
 }
