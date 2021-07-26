bb63995b2ce81a3e2d79aa81572bc74e3648791a
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2021-07-26 15:25:07 +0800

    update userDetail model for new userDetailAccEnable field

--- lib/model/UserDetail.dart
+++ lib/model/UserDetail.dart
@@ -2,14 +2,16 @@ class UserDetail {
   int userDetailId;
   String userDetailJoinedGroupID;
   String userDetailStatus;
+  int userDetailAccEnable;
 
   UserDetail(
-      {this.userDetailId, this.userDetailJoinedGroupID, this.userDetailStatus});
+      {this.userDetailId, this.userDetailJoinedGroupID, this.userDetailStatus, this.userDetailAccEnable});
 
   UserDetail.fromJson(Map<String, dynamic> json) {
     userDetailId = json['userDetail_id'];
     userDetailJoinedGroupID = json['userDetail_joinedGroupID'];
     userDetailStatus = json['userDetail_status'];
+    userDetailAccEnable = json['userDetail_accEnable'];
   }
 
   Map<String, dynamic> toJson() {
@@ -17,6 +19,7 @@ class UserDetail {
     data['userDetail_id'] = this.userDetailId;
     data['userDetail_joinedGroupID'] = this.userDetailJoinedGroupID;
     data['userDetail_status'] = this.userDetailStatus;
+    data['userDetail_accEnable'] = this.userDetailAccEnable;
     return data;
   }
 }
