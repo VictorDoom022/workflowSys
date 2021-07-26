29e6618954b0ea8cf6b62c0808e15bebca4126bc
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2021-07-26 13:59:26 +0800

    show all users in adminHome

---
+++ lib/controller/userController.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:workflow_sys/controller/setupDir.dart';
+import 'package:workflow_sys/model/User.dart';
+
+Future<List<User>> getAllUser() async{
+  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+  String token = sharedPreferences.getString("UserToken");
+
+  String stringUrl = apiURL + '/users';
+  Uri url = Uri.parse(stringUrl);
+  var response = await http.get(
+      url,
+    headers: {
+      'Accept': 'application/json',
+      'Authorization' : 'Bearer ' + token
+    }
+  );
+
+  var jsonRes = jsonDecode(response.body);
+  List<User> userList = (jsonRes as List).map((e) => User.fromJson(e)).toList();
+
+  return userList;
+}
--- lib/view/admin/adminHome.dart
+++ lib/view/admin/adminHome.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:workflow_sys/controller/userController.dart';
+import 'package:workflow_sys/model/User.dart';
 
 class adminHome extends StatefulWidget {
   const adminHome({Key key}) : super(key: key);
@@ -15,7 +17,46 @@ class _adminHomeState extends State<adminHome> {
       appBar: CupertinoNavigationBar(
         middle: Text('Home'),
       ),
-      body: Container(),
+      body: StreamBuilder<Object>(
+        initialData: getAllUser(),
+        builder: (context, snapshot){
+          return FutureBuilder<List<User>>(
+            future: getAllUser(),
+            builder: (context, snapshot){
+              if(snapshot.hasError) print(snapshot.error);
+
+              if(snapshot.hasData){
+                return userItem(users: snapshot.data);
+              }else{
+                return Center(child: CupertinoActivityIndicator(radius: 12));
+              }
+            },
+          );
+        },
+      ),
     );
   }
 }
+
+class userItem extends StatelessWidget {
+
+  final List<User> users;
+
+  const userItem({Key key, this.users}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        itemCount: users.length,
+        itemBuilder: (context, index){
+          return ListTile(
+            title: Text(users[index].name),
+            subtitle: Text(users[index].email),
+          );
+        }
+    );
+  }
+}
+
