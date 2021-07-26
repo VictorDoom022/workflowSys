f2fa2262e3803e16a79366bf732b871ded2ebf67
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2021-07-26 15:01:29 +0800

    use Http instead of Dio

--- lib/controller/authController.dart
+++ lib/controller/authController.dart
@@ -1,4 +1,4 @@
-import 'package:dio/dio.dart';
+import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -8,48 +8,38 @@ import 'package:http/http.dart' as http;
 
 void login(BuildContext context, String email, String password) async {
 
-  try{
-    var dio = new Dio();
-
-    var response = await dio.post(
-        apiURL + "/login",
-        data: {'email' : email, 'password' : password},
-        options: Options(
-            responseType: ResponseType.json,
-            sendTimeout: 5000,
-            headers: {
-              'Accept': 'application/json',
-            }
-        )
-    );
-
-    if(response.statusCode == 200){
-      AuthReceiver authReceiver = AuthReceiver.fromJson(response.data);
-
-      SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-      sharedPreferences.setString("UserToken", authReceiver.token);
-      sharedPreferences.setInt("UserID", authReceiver.user.id);
-      sharedPreferences.setString("UserEmail", authReceiver.user.email);
-      sharedPreferences.setString("UserName", authReceiver.user.name);
-      sharedPreferences.setString("UserPosition", authReceiver.user.position);
-
-      if(authReceiver.user.position == "admin"){
-        Navigator.pushReplacementNamed(context, '/adminHome');
-      }else{
-        // Navigate to user home page
+  String stringUrl = apiURL + '/login';
+  Uri url = Uri.parse(stringUrl);
+  var response = await http.post(
+      url,
+      body: {'email' : email, 'password' : password},
+      headers: {
+        'Accept': 'application/json',
       }
+  );
 
-    }else if(response.statusCode == 201){
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('These credentials do not match our records.')));
+  if(response.statusCode == 200){
+    AuthReceiver authReceiver = AuthReceiver.fromJson(jsonDecode(response.body));
+
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.setString("UserToken", authReceiver.token);
+    sharedPreferences.setInt("UserID", authReceiver.user.id);
+    sharedPreferences.setString("UserEmail", authReceiver.user.email);
+    sharedPreferences.setString("UserName", authReceiver.user.name);
+    sharedPreferences.setString("UserPosition", authReceiver.user.position);
+
+    if(authReceiver.user.position == "admin"){
+      Navigator.pushReplacementNamed(context, '/adminHome');
     }else{
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Somethings went wrong. Code:' + response.statusCode.toString())));
+      // Navigate to user home page
     }
-  }on DioError catch(e){
-    if(e.response.statusCode == 422){
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Invalid format!')));
-    }else{
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Somethings went wrong. Code' + e.response.statusCode.toString())));
-    }
+
+  }else if(response.statusCode == 201){
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('These credentials do not match our records.')));
+  }else if(response.statusCode == 422){
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Invalid input/credentials')));
+  }else{
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Somethings went wrong. Code:' + response.statusCode.toString())));
   }
 }
 
