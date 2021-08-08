959355fda581891699d73471251d464e795532de
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2021-08-08 23:41:36 +0800

    apply search function in taskCompleted page

--- lib/view/user/tasks/taskCompleted.dart
+++ lib/view/user/tasks/taskCompleted.dart
@@ -29,6 +29,7 @@ class _taskCompletedState extends State<taskCompleted> {
   RefreshController refreshController = RefreshController(initialRefresh: false);
 
   Future<List<Task>> futureTaskAssignedList;
+  String searchKeyWord="";
 
   @override
   void initState() {
@@ -45,33 +46,68 @@ class _taskCompletedState extends State<taskCompleted> {
     refreshController.refreshCompleted();
   }
 
+  Future<List<Task>> searchList() async {
+    List<Task> listTask = await futureTaskAssignedList;
+    List<Task> searchList = [];
+
+    if(searchKeyWord == ""){
+      searchList = listTask;
+    }else {
+      for(int i=0; i < listTask.length; i++){
+        if(listTask[i].taskName.toLowerCase().contains(searchKeyWord.toLowerCase()) == true){
+          searchList.add(listTask[i]);
+        }
+      }
+    }
+
+    return searchList;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CupertinoNavigationBar(
         middle: Text('Completed Task'),
       ),
-      body: SmartRefresher(
-        controller: refreshController,
-        enablePullDown: true,
-        header: BezierCircleHeader(),
-        onRefresh: getTaskCompletedData,
-        child: FutureBuilder<List<Task>>(
-          future: futureTaskAssignedList,
-          builder: (context, snapshot){
-            if(snapshot.hasError) print(snapshot.error);
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CupertinoSearchTextField(
+              onChanged: (value){
+                setState(() {
+                  searchKeyWord = value;
+                });
+                searchList();
+              },
+            ),
+          ),
+          Expanded(
+            child: SmartRefresher(
+              controller: refreshController,
+              enablePullDown: true,
+              header: BezierCircleHeader(),
+              onRefresh: getTaskCompletedData,
+              child: FutureBuilder<List<Task>>(
+                future: searchKeyWord=="" ? futureTaskAssignedList : searchList(),
+                builder: (context, snapshot){
+                  if(snapshot.hasError) print(snapshot.error);
 
-            if(snapshot.hasData){
-              if(snapshot.data.toString() != "[]"){
-                return taskItem(userReceiver: userReceiver, teamID: teamID, listTask: snapshot.data);
-              }else{
-                return Center(child: Text('No task completed'));
-              }
-            }else{
-              return Center(child: CupertinoActivityIndicator(radius: 12));
-            }
-          },
-        ),
+                  if(snapshot.hasData){
+                    if(snapshot.data.toString() != "[]"){
+                      return taskItem(userReceiver: userReceiver, teamID: teamID, listTask: snapshot.data);
+                    }else{
+                      return Center(child: Text('No task completed'));
+                    }
+                  }else{
+                    return Center(child: CupertinoActivityIndicator(radius: 12));
+                  }
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
