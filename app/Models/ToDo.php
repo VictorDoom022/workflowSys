c335e46d38b7ec06e9df629b2445b809fd072483
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2021-08-09 11:09:39 +0800

    model, migrations & controller for todo

---
+++ app/Models/ToDo.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ToDo extends Model
+{
+    use HasFactory;
+
+    protected $table = 'todo';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'todo_name',
+        'todo_desc',
+        'todo_startDate',
+        'todo_dueDate',
+        'todo_statusMsg',
+        'todo_status'
+    ];
+}
