7195fc5448bec2751716175986406ba7139e2f19
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2021-08-22 22:20:39 +0800

    Install bootstrap vue

--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+
 new Vue({
   router,
   render: h => h(App)
