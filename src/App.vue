9acff2a18b8b14e837fdf0f449642cf5ebc7e27c
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2021-08-22 23:05:22 +0800

    import bootstrap css & simple layout for login page

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
 </template>
 
 <style>
+@import url('./assets/bootstrap.css');
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/views/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="d-flex justify-content-center">
+    <div class="card">
+        <div class="card-body">
+            <h1 class="card-title">Login</h1>
+            
+            <div class="mb-3">
+                <label class="form-label">Email</label>
+                <input type="email" class="form-control">
+            </div>
+
+            <div class="mb-3">
+                <label class="form-label">Password</label>
+                <input type="password" class="form-control">
+            </div>
+
+            <div class="mb-3">
+                <button class="btn btn-primary">Login</button>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
