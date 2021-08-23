e670e71d915643e56099c569044de6458b5ff70f
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2021-08-23 10:30:29 +0800

    install vueX

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
 
@@ -14,6 +15,7 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
 Vue.use(VueAxios, axios)
+Vue.use(Vuex)
 
 new Vue({
   router,
