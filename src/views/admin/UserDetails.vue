13dd0670967faea5eb8aeb8145eb77450faf5e51
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2021-08-23 22:07:13 +0800

    view user details

--- src/views/admin/UserDetails.vue
+++ src/views/admin/UserDetails.vue
@@ -1,16 +1,77 @@
 <template>
     <div>
         <AdminTopNav />
-        {{ userID }}
+        <div class="text-start mt-3 mx-2">
+            <b-button variant="outline-dark" @click="navigateBack">back</b-button>
+        </div>
+        <b-container>
+            <div class="card mt-3 px-5 justify-content-md-center">
+                <table class="table table-striped table-hover mt-2">
+                    <thead class="table-dark">
+                        <tr>
+                            <th class="text-end">Type</th>
+                            <td class="text-start">Info</td>
+                        </tr>
+                    </thead>
+                    <tr>
+                        <th class="text-end">Name</th>
+                        <td class="text-start">{{ user.name }}</td>
+                    </tr>
+                    <tr>
+                        <th class="text-end">Email</th>
+                        <td class="text-start">{{ user.email }}</td>
+                    </tr>
+                    <tr>
+                        <th class="text-end">Create at</th>
+                        <td class="text-start">{{ user.created_at }}</td>
+                    </tr>
+                    <tr>
+                        <th class="text-end">Account Status</th>
+                        <td class="text-start">{{ userDetail.userDetail_status }}</td>
+                    </tr>
+                    <tr>
+                        <th class="text-end">Account enabled</th>
+                        <td class="text-start">{{ userDetail.userDetail_accEnable }}</td>
+                    </tr>
+                </table>
+            </div>
+        </b-container>
+        
     </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import storeData from '../../functions/storeData'
 import AdminTopNav from '../../components/admin/AdminTopNav.vue'
 
 export default {
     props: ['userID'],
     components: { AdminTopNav },
+    data() {
+        return {
+            user: null,
+            userDetail: null,
+        }
+    },
+    mounted() {
+        Vue.axios({
+            url: 'http://localhost:8000/api/users/'+  this.userID,
+            method: 'GET',
+            headers: {
+                Authorization : 'Bearer ' + storeData.state.userData['token'],
+                'Content-Type': 'application/json',
+            },
+        }).then((response) => {
+            this.user = response.data['user']
+            this.userDetail = response.data['userDetail']
+        })
+    },
+    methods: {
+        navigateBack(){
+            this.$router.go(-1)
+        }
+    }
 }
 </script>
 
