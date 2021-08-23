49d0939e229c3d81113d1c210395953338fe082f
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2021-08-23 11:34:53 +0800

    placeholder page for adminHome

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../views/Login.vue'
+import AdminHome from '../views/admin/AdminHome.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Login',
     component: Login
+  },
+  {
+    path: '/adminHome',
+    name: 'AdminHome',
+    component: AdminHome
   },
 ]
 
