97d00159e9c5869853e7d5eeccc877d2a598f1a8
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2021-08-23 17:38:48 +0800

    display all user

--- src/views/admin/AdminHome.vue
+++ src/views/admin/AdminHome.vue
@@ -1,15 +1,51 @@
 <template>
   <div>
       <AdminTopNav />
-      <h1>Admin</h1>
+      <h1>User Lists</h1>
+
+      <b-container>
+          <b-row>
+              <b-col md="4" v-for="(user, index) in users" :key="user.id">
+                  <div class="card border-dark mb-2" style="text-align:left; min-height:100px">
+                      <div class="card-body">
+                          <h5 class="card-title">{{ user.name }}</h5>
+                          <h6 class="card-subtitle">{{ user.email }}</h6>
+                          <p class="card-text">{{ userDetails[index].userDetail_status }}</p>
+                      </div>
+                  </div>
+              </b-col>              
+          </b-row>
+      </b-container>
   </div>
 </template>
 
 <script>
 import AdminTopNav from '../../components/admin/AdminTopNav.vue'
+import storeData from '../../functions/storeData'
+import Vue from 'vue'
 
 export default {
-    components: { AdminTopNav }
+    components: { AdminTopNav },
+    data() {
+        return {
+            users: [],
+            userDetails: [],
+        }
+    },
+    mounted() {
+        Vue.axios({
+            url: 'users',
+            method: 'GET',
+            headers: {
+                Authorization : 'Bearer ' + storeData.state.userData['token'],
+                'Content-Type': 'application/json',
+            },
+            }
+        ).then((response) => {
+            this.users = response.data['user']
+            this.userDetails = response.data['userDetail']
+        })
+    }
 }
 </script>
 
