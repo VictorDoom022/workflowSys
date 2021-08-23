39346ba804c2ce623b6a6e7defbde2d2d82a43f9
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2021-08-23 11:28:29 +0800

    wrap in form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,21 +5,21 @@
                 <div class="card-body">
                     <h1 class="card-title">Login</h1>
                     <b-alert variant="danger" v-if="error" show>{{ error }}</b-alert>
-                    <div class="mb-3">
-                        <label class="form-label">Email</label>
-                        <input type="email" v-model="email" class="form-control">
-                    </div>
+                    <form>
+                        <div class="mb-3">
+                            <label class="form-label">Email</label>
+                            <input type="email" v-model="email" class="form-control" autocomplete="on">
+                        </div>
 
-                    <div class="mb-3">
-                        <label class="form-label">Password</label>
-                        <input type="password" v-model="password" class="form-control">
-                    </div>
+                        <div class="mb-3">
+                            <label class="form-label">Password</label>
+                            <input type="password" v-model="password" class="form-control" autocomplete="current-password">
+                        </div>
 
-                    <div class="mb-3">
-                        <button class="btn btn-primary" @click="login">Login</button>
-                    </div>
-
-                    {{ token }} - {{ user }}
+                        <div class="mb-3">
+                            <button class="btn btn-primary" @click="login">Login</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
@@ -39,9 +39,6 @@ export default {
         return {
             email: '',
             password: '',
-            user: [],
-            userDetail: [],
-            token: '',
             error: null,
             isLoading: false,
         }
@@ -62,10 +59,6 @@ export default {
                 if(response.status == 201){
                     this.error = response.data['message'][0]
                 }else{
-                    this.user = response.data['user']
-                    this.userDetail = response.data['userDetail']
-                    this.token = response.data['token']
-
                     storeData.dispatch('createUserSession', response.data)
                 }
             })
