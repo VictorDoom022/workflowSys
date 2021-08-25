4ed026fc9fc57d89d409135d0d9d9d1723bd85db
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2021-08-26 01:13:08 +0800

    minor button design changes

--- src/views/user/UserHome.vue
+++ src/views/user/UserHome.vue
@@ -11,8 +11,8 @@
           <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
             <h1 class="h2">Groups</h1>
             <div class="btn-toolbar mb-2 mb-md-0">
-              <button type="button" @click="showJoinGroupDialog()" class="btn btn-sm btn-outline-secondary mx-1">Join Group</button>
-              <button type="button" @click="showEnterGroupNameDialog()" class="btn btn-sm btn-outline-success mx-1">Create Group</button>
+              <button type="button" @click="showJoinGroupDialog()" class="btn btn-sm btn-primary mx-1">Join Group</button>
+              <button type="button" @click="showEnterGroupNameDialog()" class="btn btn-sm btn-success mx-1">Create Group</button>
             </div>
           </div>
 
