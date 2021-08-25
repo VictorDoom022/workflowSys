b50481ad367add2aa5701a9b8a472953f09f1e50
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2021-08-25 14:44:22 +0800

    remove gap within grids & remove resposnsive stuffs

--- src/views/user/UserHome.vue
+++ src/views/user/UserHome.vue
@@ -24,30 +24,13 @@ export default {
 /* https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Grid_Layout/Grid_Template_Areas */
 .main{
   display: grid;
-  grid-template-columns: repeat(12, 1fr);
-  grid-auto-rows: minmax(100px, auto);
+  grid-template-columns: 0fr 3fr;
+  gap: 0em;
   grid-template-areas:
-    /* "hd hd hd hd   hd   hd   hd   hd   hd" */
-    "sd sd sd sd main main main main main main main main"
-    /* "ft ft ft ft   ft   ft   ft   ft   ft"; */
+    "sd main"
 }
 
 main{
   grid-area: main;
-  margin-left: -100px;
-}
-
-/* md */
-@media only screen and (max-width: 768px) {
-  main{
-    margin-left: -50px ;
-  }
-}
-
-/* sm */
-@media only screen and (max-width: 576px) {
-  main{
-    margin-left: -30px ;
-  }
 }
 </style>
