22cbb51e8892a56e8afbcc5ec44f290661d0a347
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2021-08-25 15:31:07 +0800

    display all groups

--- src/views/user/UserHome.vue
+++ src/views/user/UserHome.vue
@@ -7,20 +7,53 @@
           <b-button variant="primary mr-1">Join Group</b-button>
           <b-button variant="success">Create Group</b-button>
         </b-jumbotron>
+
+        <div class="container-fluid">
+          <b-row>
+            <b-col md="12" class="mb-3">
+                <input type="text" class="form-control" placeholder="Search groups...">
+            </b-col>
+
+            <b-col md="4" v-for="group in groups" :key="group.id">
+              <div class="card border-dark mb-2" style="text-align:left; min-height:100px">
+                    <div class="card-body">
+                        <h4 class="card-title text-center">{{ group.group_name }}</h4>
+                    </div>
+                </div>
+            </b-col>  
+          </b-row>
+        </div>
+
       </main>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
 import UserSideNav from '../../components/user/UserSideNav.vue'
+import loggedInUserData from '../../functions/loggedInUserData'
 
 export default {
     components: { UserSideNav },
     data() {
       return {
-        
+        groups: [],
       }
     },
+    mounted() {
+        Vue.axios({
+            url: 'http://localhost:8000/api/group/userID/' + loggedInUserData.state.userData['user'].id,
+            method: 'POST',
+            headers: {
+                Authorization : 'Bearer ' + loggedInUserData.state.userData['token'],
+                'Content-Type': 'application/json',
+            },
+            }
+        ).then((response) => {
+            this.groups = response.data
+            console.log(this.groups)
+        })
+    }
 }
 </script>
 
@@ -36,5 +69,9 @@ export default {
 
 main{
   grid-area: main;
+}
+
+.card{
+  cursor: pointer;
 }
 </style>
