d6b61632c74900d36380aa3a43eafca83bd2c958
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2021-08-26 09:35:21 +0800

    show loading when data is loading in TeamList

--- src/views/user/TeamList.vue
+++ src/views/user/TeamList.vue
@@ -5,7 +5,9 @@
             <div class="row">
                 <UserSideNav />
 
-                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <Loading v-if="teamList.length ==0"/>
+
+                <main v-if="teamList.length !=0" class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                         <h1 class="h2">
                             <span @click="navigateBack()" style="cursor:pointer">
@@ -48,10 +50,11 @@ import Vue from 'vue'
 import UserSideNav from '../../components/user/UserSideNav.vue'
 import UserTopNav from '../../components/user/UserTopNav.vue'
 import loggedInUserData from '../../functions/loggedInUserData'
+import Loading from '../../components/Loading.vue'
 
 export default {
     props: ['groupID'],
-    components: { UserSideNav, UserTopNav },
+    components: { UserSideNav, UserTopNav, Loading },
     data() {
         return {
             groupDetail: [],
