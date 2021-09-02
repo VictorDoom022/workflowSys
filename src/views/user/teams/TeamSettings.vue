5c81d2648d735665d31f150b81b8c722f44358a4
unknown <victordoom022@gmail.com>
2021-09-02 19:19:22 +0800

    team settings placeholder

--- src/views/user/teams/TeamSettings.vue
+++ src/views/user/teams/TeamSettings.vue
@@ -16,7 +16,107 @@
                     </div>
 
                     <div class="container">
-                        
+                        <div class="row justify-content-center">
+                            <div class="col-12 col-lg-10 col-xl-8 mx-auto">
+                                <div class="my-4">
+
+                                    <strong class="mb-0">General</strong>
+
+                                    <div class="list-group mb-5 shadow">
+                                        <div class="list-group-item">
+                                            <div class="row align-items-center">
+                                                <div class="col">
+                                                    <strong class="mb-0">View members</strong>
+                                                    <p class="text-muted mb-0">View all members in the team</p>
+                                                </div>
+                                                <div class="col-auto">
+                                                    <div class="custom-control custom-switch">
+                                                        <b-icon class="mr-1" icon="chevron-right"></b-icon>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <strong v-if="isAdmin" class="mb-0">Admin</strong>
+
+                                    <div v-if="isAdmin" class="list-group mb-5 shadow">
+                                        <div class="list-group-item">
+                                            <div class="row align-items-center">
+                                                <div class="col">
+                                                    <strong class="mb-0">Rename team</strong>
+                                                </div>
+                                                <div class="col-auto">
+                                                    <div class="custom-control custom-switch">
+                                                        <button class="btn btn-outline-primary btn-sm">Rename</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="list-group-item">
+                                            <div class="row align-items-center">
+                                                <div class="col">
+                                                    <strong class="mb-0">Add member to team</strong>
+                                                    <p class="text-muted mb-0">Add members from Group to your team</p>
+                                                </div>
+                                                <div class="col-auto">
+                                                    <div class="custom-control custom-switch">
+                                                        <b-icon class="mr-1" icon="chevron-right"></b-icon>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="list-group-item">
+                                            <div class="row align-items-center">
+                                                <div class="col">
+                                                    <strong class="mb-0">Remove member from team</strong>
+                                                    <p class="text-muted mb-0">Task related to user will be deleted</p>
+                                                </div>
+                                                <div class="col-auto">
+                                                    <div class="custom-control custom-switch">
+                                                        <b-icon class="mr-1" icon="chevron-right"></b-icon>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <strong class="mb-0 text-danger">Danger Zone</strong>
+
+                                    <div class="list-group mb-5 shadow">
+                                        <div v-if="isAdmin" class="list-group-item">
+                                            <div class="row align-items-center">
+                                                <div class="col">
+                                                    <strong class="mb-0">Delete team</strong>
+                                                    <p class="text-muted mb-0">Nuke everything related to this this</p>
+                                                </div>
+                                                <div class="col-auto">
+                                                    <div class="custom-control custom-switch">
+                                                        <button class="btn btn-outline-danger btn-sm">Delete</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="list-group-item">
+                                            <div class="row align-items-center">
+                                                <div class="col">
+                                                    <strong class="mb-0">Quit team</strong>
+                                                    <p class="text-muted mb-0">Task related to you will be deleted</p>
+                                                </div>
+                                                <div class="col-auto">
+                                                    <div class="custom-control custom-switch">
+                                                        <button class="btn btn-outline-danger btn-sm">Quit</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </main>
             </div>
@@ -37,13 +137,29 @@ export default {
     components: { UserSideNav, UserTopNav, Loading, SelectMember },
     data() {
         return {
-
+            isAdmin: false,
         }
     },
     mounted() {
-
+        this.checkIsUserAdmin()
     },
     methods: {
+        checkIsUserAdmin(){
+             Vue.axios({
+                url: '/group/checkIsUserAdminByTeamID',
+                method: 'POST',
+                headers: {
+                    Authorization : 'Bearer ' + loggedInUserData.state.userData['token'],
+                    'Content-Type': 'application/json',
+                },
+                data: {
+                    userID : loggedInUserData.state.userData['user'].id, 
+                    teamID : this.teamID
+                }
+            }).then((response) => {
+                this.isAdmin = response.data['message']
+            })
+        },
         navigateBack(){
             this.$router.go(-1)
         },
