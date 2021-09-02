5d99d7c2de0758511ac99ab9f2eb56ca24a16d2c
unknown <victordoom022@gmail.com>
2021-09-02 12:23:40 +0800

    show taskList list

--- src/views/user/teams/TaskListList.vue
+++ src/views/user/teams/TaskListList.vue
@@ -5,13 +5,15 @@
             <div class="row">
                 <UserSideNav />
 
-                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <Loading v-if="taskListList.length == 0"/>
+
+                <main v-if="taskListList.length !=0" class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                         <h1 class="h2">
                             <span @click="navigateBack()" style="cursor:pointer">
                                 <b-icon class="mr-1" icon="caret-left-fill"></b-icon>
                             </span>
-                            Team
+                            Team ({{ teamDetail.team_name }})
                         </h1>
                         <div class="btn-toolbar mb-2 mb-md-0">
                             <button type="button" class="btn btn-sm btn-primary mx-1">Settings</button>
@@ -19,7 +21,15 @@
                     </div>
 
                     <div class="container-fluid">
-
+                        <b-row>
+                            <b-col md="4" v-for="taskList in taskListList" :key="taskList.id">
+                                <div class="card border-dark mb-2" style="text-align:left; min-height:100px">
+                                    <div class="card-body">
+                                        <h4 class="card-title text-center">{{ convertUserIDToName(taskList.taskList_userID) }}</h4>
+                                    </div>
+                                </div>
+                            </b-col> 
+                        </b-row>
                     </div>
                 </main>
             </div>
@@ -39,17 +49,57 @@ export default {
     components: { UserSideNav, UserTopNav, Loading },
     data() {
         return {
+            userData: [],
+            userDetailData: [],
             teamDetail: [],
+            taskListList: [],
+            isLoading: true,
         }
     },
     mounted() {
-
+        this.fetchTeamData()
     },
     methods: {
+        fetchTeamData(){
+            Vue.axios({
+                url: '/users',
+                method: 'GET',
+                headers: {
+                    Authorization : 'Bearer ' + loggedInUserData.state.userData['token'],
+                    'Content-Type': 'application/json',
+                },
+            }).then((response) => {
+                this.userData = response.data['user'];
+                this.userDetailData = response.data['userDetail'];
+
+                Vue.axios({
+                    url: '/team/getTeamID',
+                    method: 'POST',
+                    headers: {
+                        Authorization : 'Bearer ' + loggedInUserData.state.userData['token'],
+                        'Content-Type': 'application/json',
+                    },
+                    data: {
+                        teamID : this.teamID
+                    }
+                }).then((response) => {
+                    this.teamDetail = response.data['team']
+                    this.taskListList = response.data['taskList']
+                    this.isLoading = false
+                })
+            })
+        },
+        convertUserIDToName(userID){
+            for(var i=0; i < this.userData.length; i++){
+                if(userID == this.userData[i].id){
+                    return this.userData[i].name
+                }
+            }
+        },
         navigateBack(){
             this.$router.go(-1)
         },
-    }
+    },
 }
 </script>
 
