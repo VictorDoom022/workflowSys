eaaf2a52d57439990356685183e66013b273c086
unknown <victordoom022@gmail.com>
2021-09-03 14:54:01 +0800

    show loading when task is loading

--- src/components/user/tasks/TaskCreated.vue
+++ src/components/user/tasks/TaskCreated.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="container-fluid">
+
+        <Loading v-if="isLoading" />
+
         <b-row v-if="taskList.length">
             <b-col md="12" v-for="task in searchTask" :key="task.id">
                 <TaskItem :task="task" />
@@ -13,14 +16,16 @@
 <script>
 import Vue from 'vue'
 import loggedInUserData from '../../../functions/loggedInUserData'
+import Loading from '../../Loading.vue'
 import TaskItem from '../TaskItem.vue'
 
 export default {
     props: ['taskListID', 'searchTerm'],
-    components: { TaskItem },
+    components: { Loading, TaskItem },
     data() {
         return {
             taskList: [],
+            isLoading: true,
         }
     },
     mounted() {
@@ -39,7 +44,8 @@ export default {
                     taskListID: this.taskListID,
                 },
             }).then((response) => {
-                this.taskList = response.data;
+                this.taskList = response.data
+                this.isLoading = false
             })
         },
         navigateBack(){
