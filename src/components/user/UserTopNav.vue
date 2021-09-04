e343859097946485bd07231233a59fc1876e75e0
unknown <victordoom022@gmail.com>
2021-09-05 00:04:29 +0800

    userTopNav fixes

--- src/components/user/UserTopNav.vue
+++ src/components/user/UserTopNav.vue
@@ -4,13 +4,24 @@
             Workflow Sys
         </router-link>
 
-        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+        <b-navbar-toggle target="navbar-toggle-collapse" class="position-absolute mt-1 d-md-none">
+            <template #default="{ expanded }">
+                <b-icon v-if="expanded" icon="chevron-bar-up"></b-icon>
+                <b-icon v-else icon="chevron-bar-down"></b-icon>
+            </template>
+        </b-navbar-toggle>
 
         <input class="form-control form-control-dark w-100" v-model="searchWord" @input="searchData()" type="text" placeholder="Search" aria-label="Search">
 
-        <div class="navbar-nav">
+        <b-collapse id="navbar-toggle-collapse" class="navbar-nav mobile" is-nav>
+            <div class="nav-item text-nowrap">
+                <router-link :to="{ name: 'UserHome' }" class="nav-link px-3" style="cursor:pointer;">Home</router-link>
+                <router-link :to="{ name: 'TodoList' }" class="nav-link px-3" style="cursor:pointer;">Todo</router-link>
+                <a class="nav-link px-3" style="cursor:pointer;" @click="logout()">Sign out</a>
+            </div>
+        </b-collapse>
+
+        <div class="navbar-nav desktop">
             <div class="nav-item text-nowrap">
                 <a class="nav-link px-3" style="cursor:pointer;" @click="logout()">Sign out</a>
             </div>
@@ -79,5 +90,15 @@ export default {
 .form-control-dark:focus {
   border-color: transparent;
   box-shadow: 0 0 0 3px rgba(255, 255, 255, .25);
+}
+
+.mobile .router-link-active {
+    text-shadow: 1px 1px 7px rgb(235, 221, 221);
+}
+
+@media screen and (max-width: 768px) {
+  .desktop {
+      display: none;
+  }
 }
 </style>
