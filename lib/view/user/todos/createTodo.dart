808cf4ef21f7215be14a4f70b5e13c4e65d5ccfb
victordoom022 <victordoom022@gmail.com>
2021-09-09 17:44:23 +0800

    use new coding implementation in createTodo

--- lib/view/user/todos/createTodo.dart
+++ lib/view/user/todos/createTodo.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 import 'package:workflow_sys/controller/todoController.dart';
+import 'package:workflow_sys/view/misc/TaskForm.dart';
 import 'package:workflow_sys/view/misc/loadingScreen.dart';
 import 'package:workflow_sys/view/user/userNavDrawer.dart';
 
@@ -53,197 +54,118 @@ class _createTodoState extends State<createTodo> {
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisSize: MainAxisSize.min,
             children: [
-              Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        'Todo basic info',
-                        style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Row(
-                        children: [
-                          Expanded(
-                            flex: 3,
-                            child: Text('Name'),
-                          ),
-                          Expanded(
-                            flex: 9,
-                            child: TextField(
-                              controller: todoNameController,
-                              decoration: textFieldInputDecoration,
-                            ),
-                          )
-                        ],
-                      ),
-                      Divider(),
-                      Row(
-                        children: [
-                          Expanded(
-                            flex: 3,
-                            child: Text('Description'),
-                          ),
-                          Expanded(
-                            flex: 9,
-                            child: TextField(
-                              controller: todoDescController,
-                              maxLines: 10,
-                              decoration: textFieldInputDecoration,
-                            ),
-                          )
-                        ],
-                      ),
-                      Divider(),
-                      Row(
-                        children: [
-                          Expanded(
-                            flex: 3,
-                            child: Text('Status'),
-                          ),
-                          Expanded(
-                            flex: 9,
-                            child: TextField(
-                              controller: todoStatusMsgController,
-                              maxLines: 1,
-                              minLines: 1,
-                              decoration: textFieldInputDecoration,
-                            ),
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        'Extras',
-                        style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Row(
-                        children: [
-                          Expanded(
-                            flex: 3,
-                            child: Text('Start Date'),
-                          ),
-                          Expanded(
-                              flex: 9,
-                              child: TextButton(
-                                child: Text(todoStartDate==null ? 'Select date' : todoStartDate.toString()),
-                                onPressed: (){
-                                  showDialog(
-                                      context: context,
-                                      builder: (_){
-                                        return Dialog(
-                                          child: SfDateRangePicker(
-                                            enablePastDates: false,
-                                            showActionButtons: true,
-                                            onSubmit: (date){
-                                              Navigator.of(context).pop();
-                                              setState(() {
-                                                todoStartDate = date;
-                                              });
-                                            },
-                                            onCancel: (){
-                                              Navigator.of(context).pop();
-                                            },
-                                          ),
-                                        );
-                                      }
-                                  );
-                                },
-                              )
-                          )
-                        ],
-                      ),
-                      Divider(),
-                      Row(
-                        children: [
-                          Expanded(
-                            flex: 3,
-                            child: Text('Due Date'),
-                          ),
-                          Expanded(
-                            flex: 9,
-                            child: TextButton(
-                              child: Text(todoDueDate==null ? 'Select date' : todoDueDate.toString()),
-                              onPressed: (){
-                                showDialog(
-                                    context: context,
-                                    builder: (_){
-                                      return Dialog(
-                                        child: SfDateRangePicker(
-                                          showActionButtons: true,
-                                          onSubmit: (date){
-                                            Navigator.of(context).pop();
-                                            setState(() {
-                                              todoDueDate = date;
-                                            });
-                                          },
-                                          onCancel: (){
-                                            Navigator.of(context).pop();
-                                          },
-                                        ),
-                                      );
-                                    }
-                                );
-                              },
-                            ),
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SizedBox(
-                  height: 40,
-                  width: double.infinity,
-                  child: TextButton(
-                    child: Text(
-                      'Save',
-                      style: TextStyle(
-                          fontWeight: FontWeight.w300,
-                          color: Colors.white,
-                          fontSize: 18
-                      ),
+              TaskFormCard(
+                cardTitle: 'Todo basic info',
+                cardItemList: [
+                  TaskItem(
+                    itemTitle: 'Name',
+                    itemWidget: TextField(
+                      controller: todoNameController,
+                      decoration: textFieldInputDecoration,
                     ),
-                    style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            Colors.amber
-                        )
+                  ),
+                  Divider(),
+                  TaskItem(
+                    itemTitle: 'Description',
+                    itemWidget: TextField(
+                      controller: todoDescController,
+                      maxLines: 10,
+                      decoration: textFieldInputDecoration,
                     ),
-                    onPressed: () {
-                      if(todoNameController.text.isNotEmpty && todoDescController.text.isNotEmpty && todoStatusMsgController.text.isNotEmpty){
-                        LoadingScreen.showLoadingScreen(context, userCreateTodoSecondScaffoldKey);
-                        String startDate = todoStartDate.toString();
-                        String dueDate = todoDueDate.toString();
+                  ),
+                  Divider(),
+                  TaskItem(
+                    itemTitle: 'Status',
+                    itemWidget: TextField(
+                      controller: todoStatusMsgController,
+                      maxLines: 1,
+                      minLines: 1,
+                      decoration: textFieldInputDecoration,
+                    ),
+                  ),
+                ],
+              ),
+              TaskFormCard(
+                cardTitle: 'Extras',
+                cardItemList: [
+                  TaskItem(
+                    itemTitle: 'Start Date',
+                    itemWidget: TextButton(
+                      child: Text(todoStartDate==null ? 'Select date' : todoStartDate.toString()),
+                      onPressed: (){
+                        showDialog(
+                            context: context,
+                            builder: (_){
+                              return Dialog(
+                                child: SfDateRangePicker(
+                                  enablePastDates: false,
+                                  showActionButtons: true,
+                                  onSubmit: (date){
+                                    Navigator.of(context).pop();
+                                    setState(() {
+                                      todoStartDate = date;
+                                    });
+                                  },
+                                  onCancel: (){
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                              );
+                            }
+                        );
+                      },
+                    ),
+                  ),
+                  Divider(),
+                  TaskItem(
+                    itemTitle: 'Due Date',
+                    itemWidget: TextButton(
+                      child: Text(todoDueDate==null ? 'Select date' : todoDueDate.toString()),
+                      onPressed: (){
+                        showDialog(
+                            context: context,
+                            builder: (_){
+                              return Dialog(
+                                child: SfDateRangePicker(
+                                  showActionButtons: true,
+                                  onSubmit: (date){
+                                    Navigator.of(context).pop();
+                                    setState(() {
+                                      todoDueDate = date;
+                                    });
+                                  },
+                                  onCancel: (){
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                              );
+                            }
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              Divider(),
+              TaskFormButton(
+                buttonText: 'Save',
+                buttonColor: Colors.amber,
+                buttonFunction: () {
+                  if(todoNameController.text.isNotEmpty && todoDescController.text.isNotEmpty && todoStatusMsgController.text.isNotEmpty){
+                    LoadingScreen.showLoadingScreen(context, userCreateTodoSecondScaffoldKey);
+                    String startDate = todoStartDate.toString();
+                    String dueDate = todoDueDate.toString();
 
-                        createNewTodo(context, todoNameController.text, todoDescController.text, todoStatusMsgController.text, startDate, dueDate).then((value) {
-                          Navigator.of(context).pop();
-                          todoNameController.clear();
-                          todoDescController.clear();
-                          todoStatusMsgController.clear();
-                        });
-                      }else{
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Name, description & status cannot be empty')));
-                      }
-                    },
-                  ),
-                ),
+                    createNewTodo(context, todoNameController.text, todoDescController.text, todoStatusMsgController.text, startDate, dueDate).then((value) {
+                      Navigator.of(context).pop();
+                      todoNameController.clear();
+                      todoDescController.clear();
+                      todoStatusMsgController.clear();
+                    });
+                  }else{
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Name, description & status cannot be empty')));
+                  }
+                },
               ),
             ],
           ),
