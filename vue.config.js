7daa11580e52c23afe78e058062ad413e796f808
unknown <victordoom022@gmail.com>
2021-10-14 14:02:14 +0800

    install & setup SCSS

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,22 @@
+// vue.config.js
+const path = require('path')
+
 module.exports = {
     devServer: {
         proxy: 'http://192.168.0.181:8000/api'
-    }
+    },
+    chainWebpack: config => {
+        const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+        types.forEach(type => addStyleResource(config.module.rule('stylus').oneOf(type)))
+    },
+}
+
+function addStyleResource (rule) {
+    rule.use('style-resource')
+        .loader('style-resources-loader')
+        .options({
+        patterns: [
+                path.resolve(__dirname, './src/styles/imports.styl'),
+            ],
+        })
 }
