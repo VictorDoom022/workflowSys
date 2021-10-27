bbe664913b69410592db4e3400924bade9552dde
unknown <victordoom022@gmail.com>
2021-10-27 14:35:40 +0800

    add & setup controller for user dashboard

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    //show current user active task count
+
+    //show user task with high priority count
+
+    //show user completed task count
+
+    //show curent task being assigend to user count (not completed)
+}
