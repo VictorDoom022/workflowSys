9c9c3ab4a3f13076ca5936ca51b5e6dbaca1e44a
unknown <victordoom022@gmail.com>
2021-10-28 16:03:07 +0800

    show other task data in dashboard

---
+++ src/views/user/TestData.vue
@@ -0,0 +1,33 @@
+<script>
+import { Line, mixins } from 'vue-chartjs'
+// to show task being updated daily
+
+// label = date
+// data = count task for that day
+export default {
+    extends: Line,
+    props: {
+        data: {
+            type: Array,
+            default: null
+        },
+    },
+    data(){
+        return {
+            chartdata: {
+                labels: ['test', 'test2'],
+                datasets: this.data
+            },
+            options: {
+                responsive : true,
+            }
+        }
+    },
+    methods: {
+
+    },
+  mounted () {
+    this.renderChart(this.chartdata, this.options)
+  }
+}
+</script>
--- src/views/user/UserDashboard.vue
+++ src/views/user/UserDashboard.vue
@@ -11,8 +11,44 @@
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                 <h1 class="h2">Dashboard</h1>
             </div>
-        </main>
 
+            <div v-if="isLoading" class="mx-auto">
+              <div class="spinner-border text-dark" role="status" style="width: 3rem; height: 3rem;">
+                  <span class="visually-hidden">Loading...</span>
+              </div>
+            </div>
+
+            <div v-if="!isLoading" class="contaier-fluid">
+                <div class="row g-1">
+                    <div class="col-lg-4 px-1">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title fw-bold text-success">Active Tasks</h5>
+                                <p class="card-text fs-3">{{ userActiveTask.length }}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 px-1">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title fw-bold text-danger">High Priority Tasks</h5>
+                                <p class="card-text fs-3">{{ userHighPriorityTask.length }}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 px-1">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title fw-bold text-primary">Assigend To You</h5>
+                                <p class="card-text fs-3">{{ taskAssignUser.length }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
       </div>
     </div>
   </div>
@@ -28,7 +64,10 @@ export default {
     components: { UserSideNav, UserTopNav },
     data() {
         return {
+            isLoading: true,
             userActiveTask: [],
+            userHighPriorityTask: [],
+            taskAssignUser: [],
         }
     },
     mounted() {
@@ -49,6 +88,41 @@ export default {
             }
             ).then((response) => {
                 this.userActiveTask = response.data
+                this.fetchUserHighPriorityTask()
+            })
+        },
+        fetchUserHighPriorityTask(){
+            Vue.axios({
+                url: '/data/getHighPriorityTask',
+                method: 'GET',
+                headers: {
+                    Authorization : 'Bearer ' + loggedInUserData.state.userData['token'],
+                    'Content-Type': 'application/json',
+                },
+                params: {
+                    userID: loggedInUserData.state.userData['user'].id,
+                },
+            }
+            ).then((response) => {
+                this.userHighPriorityTask = response.data
+                this.fetchTaskAssigendToUser()
+            })
+        },
+        fetchTaskAssigendToUser(){
+            Vue.axios({
+                url: '/data/getAssigendToUserTask',
+                method: 'GET',
+                headers: {
+                    Authorization : 'Bearer ' + loggedInUserData.state.userData['token'],
+                    'Content-Type': 'application/json',
+                },
+                params: {
+                    userID: loggedInUserData.state.userData['user'].id,
+                },
+            }
+            ).then((response) => {
+                this.taskAssignUser = response.data
+                this.isLoading = false
             })
         }
     }
