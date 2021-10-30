c217e3aa5b20f384491c440f8dfceaf7da1ad178
unknown <victordoom022@gmail.com>
2021-10-30 22:05:53 +0800

    userHome header setup

--- lib/view/user/userHome.dart
+++ lib/view/user/userHome.dart
@@ -1,5 +1,11 @@
+import 'package:draggable_home/draggable_home.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_html/shims/dart_ui_real.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:workflow_sys/view/initLoading.dart';
+import 'package:workflow_sys/view/misc/CustomDraggableHome.dart';
 import 'package:workflow_sys/view/user/userNavDrawer.dart';
 
 class userHome extends StatefulWidget {
@@ -10,11 +16,72 @@ class userHome extends StatefulWidget {
 }
 
 class _userHomeState extends State<userHome> {
+
+  GlobalKey<ScaffoldState> userHomeScaffoldKey = GlobalKey();
+  String loggedInUserName = "";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getCurrentUserName();
+  }
+
+  void getCurrentUserName() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    String? userName = sharedPreferences.getString("UserName");
+    setState(() {
+      loggedInUserName = userName!;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: userHomeScaffoldKey,
       drawer: userNavDrawer(),
-
+      body: CustomDraggableHome(
+        alwaysShowLeadingAndAction: true,
+        curvedBodyRadius: 5.0,
+        leading: IconButton(
+          icon: Icon(
+            Icons.list,
+            color: Colors.white,
+          ),
+          onPressed: (){
+            HapticFeedback.lightImpact();
+            userHomeScaffoldKey.currentState!.openDrawer();
+          },
+        ),
+        title: Text(
+          'Home',
+          style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w600
+          ),
+        ),
+        headerBackgroundColor: Colors.black,
+        headerWidget: Container(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Welcome, ' + loggedInUserName,
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50,
+                    fontWeight: FontWeight.w200,
+                ),
+              ),
+            ),
+          ),
+        ),
+        body: [
+          Container(),
+        ],
+      ),
     );
   }
 }
