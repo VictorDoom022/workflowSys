898da0ae0d796275e781c3a8d66b40d2f702ae30
unknown <victordoom022@gmail.com>
2021-11-10 13:08:59 +0800

    use card border color instead of setting TaskCard background color

--- lib/view/misc/TaskCard.dart
+++ lib/view/misc/TaskCard.dart
@@ -17,7 +17,7 @@ class TaskCard extends StatelessWidget {
 
   TaskCard({Key? key, required this.taskDate, required this.taskMonth, required this.title, required this.taskCreateUserName, required this.lastUpdatedTime, required this.lastAssignedUserName, required this.statusMsg, required this.desc, required this.color, required this.priority}) : super(key: key);
 
-  Color setCardBackgroundColor(colorID){
+  Color setCardBorderColor(colorID){
     // taskColor docs
     // 0 = default
     // 1 = blue (primary)
@@ -33,143 +33,151 @@ class TaskCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: setCardBackgroundColor(color),
-      child: Row(
-        children: [
-          Expanded(
-              flex: 2,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                        color: Colors.black54,
-                        borderRadius: BorderRadius.all(
-                            Radius.circular(10)
-                        )
-                    ),
-                    child: Text(
-                      taskDate,
-                      style: TextStyle(
-                          fontSize: 25,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w300
-                      ),
-                    ),
-                  ),
-                  Text(
-                    taskMonth,
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold
-                    ),
-                  )
-                ],
-              )
-          ),
-          Expanded(
-            flex: 10,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title,
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Wrap(
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(15.0),
+      child: Card(
+        shape: Border(
+          left: BorderSide(
+            color: setCardBorderColor(color),
+            width: 3
+          )
+        ),
+        child: Row(
+          children: [
+            Expanded(
+                flex: 2,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
                   children: [
                     Container(
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Icon(
-                            Icons.person_add,
-                            color: Colors.black54,
-                          ),
-                          SizedBox(width: 5),
-                          Text(
-                            taskCreateUserName,
-                            style: TextStyle(
-                                fontSize: 15
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(width: 10),
-                    Container(
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Icon(
-                            CupertinoIcons.person_crop_circle_fill_badge_exclam,
-                            color: Colors.black54,
-                          ),
-                          SizedBox(width: 5),
-                          Text(
-                            lastAssignedUserName,
-                            style: TextStyle(
-                                fontSize: 15
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(width: 10),
-                    Container(
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Icon(
-                            Icons.warning_amber_rounded,
-                            color: Colors.black54,
-                          ),
-                          SizedBox(width: 5),
-                          Text(
-                            convertPriorityTypeToString(priority),
-                            style: TextStyle(
-                                fontSize: 15
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(width: 10),
-                    Container(
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Icon(
-                            Icons.info_outline,
-                            color: Colors.black54,
-                          ),
-                          Text(
-                            statusMsg,
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          color: Colors.black54,
+                          borderRadius: BorderRadius.all(
+                              Radius.circular(10)
                           )
-                        ],
+                      ),
+                      child: Text(
+                        taskDate,
+                        style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w300
+                        ),
                       ),
                     ),
+                    Text(
+                      taskMonth,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold
+                      ),
+                    )
                   ],
-                ),
-                Text(
-                  desc,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                      fontSize: 16
-                  ),
-                ),
-              ],
+                )
             ),
-          ),
-        ],
+            Expanded(
+              flex: 10,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Wrap(
+                    children: [
+                      Container(
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(
+                              Icons.person_add,
+                              color: Colors.black54,
+                            ),
+                            SizedBox(width: 5),
+                            Text(
+                              taskCreateUserName,
+                              style: TextStyle(
+                                  fontSize: 15
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Container(
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(
+                              CupertinoIcons.person_crop_circle_fill_badge_exclam,
+                              color: Colors.black54,
+                            ),
+                            SizedBox(width: 5),
+                            Text(
+                              lastAssignedUserName,
+                              style: TextStyle(
+                                  fontSize: 15
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Container(
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(
+                              Icons.warning_amber_rounded,
+                              color: Colors.black54,
+                            ),
+                            SizedBox(width: 5),
+                            Text(
+                              convertPriorityTypeToString(priority),
+                              style: TextStyle(
+                                  fontSize: 15
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Container(
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(
+                              Icons.info_outline,
+                              color: Colors.black54,
+                            ),
+                            Text(
+                              statusMsg,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    desc,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                        fontSize: 16
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
