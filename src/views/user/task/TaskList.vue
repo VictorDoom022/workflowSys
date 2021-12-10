93c5d8ec8930132f5ba6aa60cb12f793f1384837
unknown <victordoom022@gmail.com>
2021-12-10 22:51:52 +0800

    set default task view to table

--- src/views/user/task/TaskList.vue
+++ src/views/user/task/TaskList.vue
@@ -66,7 +66,7 @@ export default {
     data() {
         return {
             pageDataParsed: [],
-            isCardView: true,
+            isCardView: false,
             userID: null,
             groupID: null,
             teamID: null,
