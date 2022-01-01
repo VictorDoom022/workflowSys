ce0a5f26af5717c2af9e3532f1a1e22978566b4a
unknown <victordoom022@gmail.com>
2022-01-01 22:05:31 +0800

    add sort create task fuction

--- src/components/user/tasks/TaskCreated.vue
+++ src/components/user/tasks/TaskCreated.vue
@@ -3,6 +3,20 @@
 
         <Loading v-if="isLoading" />
 
+        <div class="row mb-2">
+            <div class="col-sm-4"></div>
+            <div class="col-sm-4 pr-0">
+                <p class="text-sm-end pt-1">Sort By: </p>
+            </div>
+            <div class="col-sm-4 pl-1">
+                <select class="form-select form-select-sm" @change="sortList($event)">
+                    <option v-for="sortOptions in selectSortOptions" :key="sortOptions" :value="sortOptions">
+                        {{ sortOptions }}
+                    </option>
+                </select>
+            </div> 
+        </div>
+
         <b-row v-if="taskList.length && isCardView">
             <b-col md="6" v-for="task in searchTask" :key="task.id">
                 <TaskItem :taskPageData="taskListPageData" :task="task" :userData="userData" :userDetailData="userDetailData" />
@@ -63,6 +77,7 @@ export default {
             taskListID: null,
             taskList: [],
             isLoading: true,
+            selectSortOptions: ['Default', 'Create At Desc', 'Updated Asc', 'Updated Desc', 'Name Asc', 'Name Desc', 'Priority Asc', 'Priority Desc', 'Color Asc', 'Color Desc'],
         }
     },
     mounted() {
@@ -87,8 +102,57 @@ export default {
                 },
             }).then((response) => {
                 this.taskList = response.data
+                console.log(this.taskList)
+                this.taskList.sort((a,b) => {
+                    // return a.createAt.compareTo(b.createAt)
+                    return new Date(a.updated_at) - new Date(b.updated_at)
+                })
                 this.isLoading = false
             })
+        },
+        sortList(event){
+            console.log(event.target.value)
+            if(this.selectSortOptions[0] == event.target.value){
+                this.taskList.sort((a,b) => {
+                    return new Date(a.created_at) - new Date(b.created_at)
+                })
+            }else if(this.selectSortOptions[1] == event.target.value){
+                this.taskList.sort((a,b) => {
+                    return new Date(b.created_at) - new Date(a.created_at)
+                })
+            }else if(this.selectSortOptions[2] == event.target.value){
+                this.taskList.sort((a,b) => {
+                    return new Date(a.updated_at) - new Date(b.updated_at)
+                })
+            }else if(this.selectSortOptions[3] == event.target.value){
+                this.taskList.sort((a,b) => {
+                    return new Date(b.updated_at) - new Date(a.updated_at)
+                })
+            }else if(this.selectSortOptions[4] == event.target.value){
+                this.taskList.sort((a,b) => {
+                    return a.task_name.localeCompare(b.task_name)
+                })
+            }else if(this.selectSortOptions[5] == event.target.value){
+                this.taskList.sort((a,b) => {
+                    return b.task_name.localeCompare(a.task_name)
+                })
+            }else if(this.selectSortOptions[6] == event.target.value){
+                this.taskList.sort((a,b) => {
+                    return a.task_priority - b.task_priority
+                })
+            }else if(this.selectSortOptions[7] == event.target.value){
+                this.taskList.sort((a,b) => {
+                    return b.task_priority - a.task_priority
+                })
+            }else if(this.selectSortOptions[8] == event.target.value){
+                this.taskList.sort((a,b) => {
+                    return a.task_color - b.task_color
+                })
+            }else if(this.selectSortOptions[9] == event.target.value){
+                this.taskList.sort((a,b) => {
+                    return b.task_color - a.task_color
+                })
+            }
         },
         navigateBack(){
             this.$router.go(-1)
