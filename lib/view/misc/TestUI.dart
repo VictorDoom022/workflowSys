65079d3bfd4a72ca8f4bbe1e231c452bd0ad2981
unknown <victordoom022@gmail.com>
2022-01-06 22:23:23 +0800

    fixes for TestUI

--- lib/view/misc/TestUI.dart
+++ lib/view/misc/TestUI.dart
@@ -6,15 +6,19 @@ class TestUI extends StatefulWidget {
   final String pageTitle;
   final bool showSearchTextField;
   final TextEditingController? searchFieldController;
+  final Function(String)? onchange;
   final Widget bodyContent;
   final List<HeaderActionButton>? actions;
+  final Widget? floatingActionButton;
 
   const TestUI({Key? key,
     required this.pageTitle,
     required this.showSearchTextField,
     this.searchFieldController,
+    this.onchange,
     required this.bodyContent,
-    this.actions
+    this.actions,
+    this.floatingActionButton
   }) : super(key: key);
 
   @override
@@ -25,6 +29,8 @@ class _TestUIState extends State<TestUI> {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       body: SingleChildScrollView(
         physics: BouncingScrollPhysics(),
@@ -37,28 +43,42 @@ class _TestUIState extends State<TestUI> {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        IconButton(
-                          icon: Icon(
-                              Icons.chevron_left,
-                              size: 30,
+                    Expanded(
+                      flex: 8,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            icon: Icon(
+                                Icons.chevron_left,
+                                size: 30,
+                            ),
+                            onPressed: (){
+                              Navigator.of(context).pop();
+                            }
                           ),
-                          onPressed: (){
-                            Navigator.of(context).pop();
-                          }
-                        ),
-                        Text(
-                          widget.pageTitle,
-                          style: TextStyle(
-                              fontSize: 32,
-                              fontWeight: FontWeight.bold
+                          Text(
+                            widget.pageTitle,
+                            style: TextStyle(
+                                fontSize: screenWidth * 0.08 > 32 ? 32 : screenWidth * 0.08,
+                                fontWeight: FontWeight.bold
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                    ...widget.actions!
+                    Expanded(
+                      flex: 3,
+                      child: Wrap(
+                        spacing: 5,
+                        runSpacing: 5,
+                        children: [
+                          if(widget.actions != null)
+                            ...widget.actions!
+                        ],
+                      ),
+                    )
+
                     // for(int i=0; i < actions!.length; i++) actions![i]
                   ],
                 ),
@@ -67,6 +87,7 @@ class _TestUIState extends State<TestUI> {
             widget.showSearchTextField ? Padding(
               padding: EdgeInsets.only(top: 16, left: 16, right: 16),
               child: TextField(
+                onChanged: widget.onchange,
                 controller: widget.searchFieldController,
                 decoration: InputDecoration(
                   hintText: "Search ...",
@@ -97,6 +118,7 @@ class _TestUIState extends State<TestUI> {
           ],
         ),
       ),
+      floatingActionButton: widget.floatingActionButton,
     );
   }
 }
@@ -107,36 +129,76 @@ class HeaderActionButton extends StatelessWidget {
   final Color buttonBackgroundColor;
   final IconData buttonIcon;
   final Color buttonIconColor;
+  final VoidCallback buttonFunction;
 
-  const HeaderActionButton({Key? key, required this.buttonTitle, required this.buttonBackgroundColor, required this.buttonIcon, required this.buttonIconColor}) : super(key: key);
+  const HeaderActionButton({Key? key, required this.buttonTitle, required this.buttonBackgroundColor, required this.buttonIcon, required this.buttonIconColor, required this.buttonFunction}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(left: 8, right: 8, top: 2, bottom: 2),
-      height: 30,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-        color: buttonBackgroundColor,
-      ),
-      child: Row(
-        children: [
-          Icon(
-              buttonIcon,
-              color: buttonIconColor,
-              size: 20
-          ),
-          SizedBox(width: 2,),
-          Text(
-            buttonTitle,
-            style: TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.bold
+    return GestureDetector(
+      onTap: buttonFunction,
+      child: Container(
+        padding: EdgeInsets.only(left: 8, right: 8, top: 2, bottom: 2),
+        height: 30,
+        constraints: BoxConstraints(
+          maxWidth: 150
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          color: buttonBackgroundColor,
+        ),
+        child: Row(
+          children: [
+            Icon(
+                buttonIcon,
+                color: buttonIconColor,
+                size: 20
             ),
-          ),
-        ],
+            SizedBox(width: 2,),
+            Text(
+              buttonTitle,
+              style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
+class CupertinoCustomSearchBar extends StatelessWidget {
+
+  final Function(String) onChanged;
+
+  const CupertinoCustomSearchBar({Key? key, required this.onChanged}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        hintText: "Search ...",
+        hintStyle: TextStyle(
+          color: Colors.grey.shade600,
+        ),
+        prefixIcon: Icon(
+          Icons.search,
+          color: Colors.grey.shade600,
+          size: 20,
+        ),
+        filled: true,
+        fillColor: Colors.grey.shade100,
+        contentPadding: EdgeInsets.all(8),
+        enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20),
+            borderSide: BorderSide(
+                color: Colors.grey.shade100
+            )
+        ),
+      ),
+    );
+  }
+}
