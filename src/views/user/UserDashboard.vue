64930e90ba6849ea81a7dae2d3c0e18fb6094c01
unknown <victordoom022@gmail.com>
2022-01-17 15:14:17 +0800

    user dashboard UI changes

--- src/views/user/UserDashboard.vue
+++ src/views/user/UserDashboard.vue
@@ -20,58 +20,58 @@
 
             <div v-if="!isLoading" class="contaier-fluid">
                 <div class="row g-1">
+                    <div class="col px-1">
+                        <div class="card bg-darkBlue rounded-0">
+                            <div class="card-body">
+                                <h5 class="card-title fw-bold text-white">Total Tasks</h5>
+                                <p class="card-text text-white fs-3">{{ totalTaskCount }}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col px-1">
+                        <div class="card bg-lightGreen rounded-0">
+                            <div class="card-body">
+                                <h5 class="card-title fw-bold text-white">Active Tasks</h5>
+                                <p class="card-text text-white fs-3">{{ activeTaskCount }}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col px-1">
+                        <div class="card bg-lightOrange rounded-0">
+                            <div class="card-body">
+                                <h5 class="card-title fw-bold text-white">Tasks Assigned</h5>
+                                <p class="card-text text-white fs-3">{{ assignedToUserTaskCount }}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col px-1">
+                        <div class="card bg-pink rounded-0">
+                            <div class="card-body">
+                                <h5 class="card-title fw-bold text-white">High Priority Tasks</h5>
+                                <p class="card-text text-white fs-3">{{ highPriorityTaskCount }}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col px-1">
+                        <div class="card bg-blue rounded-0">
+                            <div class="card-body">
+                                <h5 class="card-title fw-bold text-white">Completed Task</h5>
+                                <p class="card-text text-white fs-3">{{ completedTaskCount }}</p>
+                            </div>
+                        </div>
+                    </div>
+
                     <div class="col-lg-12 px-1">
                         <div class="card rounded-0">
                             <div class="card-body">
-                                <h5 class="card-title fw-bold text-info">Total Tasks</h5>
-                                <p class="card-text fs-3">{{ totalTaskCount }}</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 px-1">
-                        <div class="card rounded-0">
-                            <div class="card-body">
-                                <h5 class="card-title fw-bold text-success">Active Tasks</h5>
-                                <p class="card-text fs-3">{{ activeTaskCount }}</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 px-1">
-                        <div class="card rounded-0">
-                            <div class="card-body">
-                                <h5 class="card-title fw-bold text-secondary">Tasks Assigned To You</h5>
-                                <p class="card-text fs-3">{{ assignedToUserTaskCount }}</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 px-1">
-                        <div class="card rounded-0">
-                            <div class="card-body">
-                                <h5 class="card-title fw-bold text-danger">High Priority Tasks</h5>
-                                <p class="card-text fs-3">{{ highPriorityTaskCount }}</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-3 px-1">
-                        <div class="card rounded-0">
-                            <div class="card-body">
-                                <h5 class="card-title fw-bold text-primary">Completed Task</h5>
-                                <p class="card-text fs-3">{{ completedTaskCount }}</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-12 px-1">
-                        <div class="card rounded-0">
-                            <div class="card-body">
-                                <h5 class="card-title fw-bold">Active Task / Completed Task</h5>
+                                <h5 class="card-title fw-bold">Task Complete Percentage</h5>
                                 <b-progress :max="totalTaskCount">
-                                    <b-progress-bar variant="success" :value="activeTaskCount" :label="`${((activeTaskCount / totalTaskCount) * 100).toFixed(2)}%`" animated show-progress></b-progress-bar>
-                                    <b-progress-bar variant="primary" :value="completedTaskCount" :label="`${((completedTaskCount / totalTaskCount) * 100).toFixed(2)}%`"  show-progress></b-progress-bar>
+                                    <b-progress-bar variant="success" :value="completedTaskCount" :label="`${((completedTaskCount / totalTaskCount) * 100).toFixed(2)}%`" animated show-progress></b-progress-bar>
+                                    <b-progress-bar variant="primary" :value="activeTaskCount"></b-progress-bar>
                                 </b-progress>
                             </div>
                         </div>
@@ -244,4 +244,25 @@ export default {
     background-color: #f8f9fa;
     transition: .5s ease;
 }
+
+.bg-darkBlue{
+    background-color:#4f4fae;
+}
+
+.bg-lightGreen{
+    background-color: #2ed694;
+}
+
+.bg-lightOrange{
+    background-color:#fe6f5e;
+}
+
+.bg-pink{
+    background-color: #ff3f78;
+}
+
+.bg-blue{
+    background-color: #475afe;
+}
+
 </style>
